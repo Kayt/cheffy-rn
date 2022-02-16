999ed1a4361386fc0a671893c8674c0375e7ad01
Achim Munene <achim_munene@hotmail.com>
2022-02-16 16:36:57 +0200

    adds header button

--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import { MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailSceen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -17,6 +20,17 @@ MealDetailSceen.navigationOptions = (navigatioData) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favourite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favourite");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
